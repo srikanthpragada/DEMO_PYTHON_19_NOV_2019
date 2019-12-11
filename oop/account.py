d0dfb22d5b080ec9741cca8be83532d307cae136
srikanthpragada <srikanthpragada@gmail.com>
2019-12-11 08:28:41 +0530

    11-DEC

--- oop/account.py
+++ oop/account.py
@@ -1,3 +1,11 @@
+class InvalidAmountError(Exception):
+    def __init__(self, amount):
+        self.message = f'Invalid transaction amount : {amount}'
+
+    def __str__(self):
+        return self.message
+
+
 class Account:
     # class attribute or static variable
     minbal = 5000
@@ -21,6 +29,9 @@ class Account:
         print("Balance     :", self.balance)
 
     def deposit(self, amount):
+        if amount <= 0:
+            raise InvalidAmountError(amount)
+
         self.balance += amount
         self.transactions.append(('d', amount, self.balance))
 
@@ -37,14 +48,16 @@ class Account:
         return self.balance - Account.minbal
 
 
-print(Account.getminbal())  # call static method
+# print(Account.getminbal())  # call static method
 
 a1 = Account(101, "Mr. Steve", 20000)  # Object
 a2 = Account(102, "Mr. Larry")
-a2.deposit(10000)
-print(a2.availbal)   # Property
+try:
+    a2.deposit(-10000)
+    print(a2.availbal)  # Property
+    a2.withdraw(300000)
 
-a2.withdraw(3000)
-
-for ttype, tamt, bal in a2.transactions:
-    print(ttype, tamt)
+    for ttype, tamt, bal in a2.transactions:
+        print(ttype, tamt)
+except Exception as ex:
+    print(ex)
